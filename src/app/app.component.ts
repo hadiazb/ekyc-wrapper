e3c2c2db1c710b66c3fa9d7408fc3fd0044574e8
hadiazb <hugo.diaz@bitsamericas.com>
2025-01-24 12:16:47 -0500

    start project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { EkycComponent } from './components/ekyc/ekyc.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [EkycComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ekyc-wrapper';
